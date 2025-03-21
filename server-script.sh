bec71f7ab14190a6eba89a494e6bf58dae6db481
nikhilk1234 <104249202+nikhilk1234@users.noreply.github.com>
2025-03-21 19:01:58 +0530

    Update server-script.sh

--- server-script.sh
+++ server-script.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
 sudo yum install git -y
-sudo yum install java -y
-echo "hi"
+sudo yum install docker -y
+sudo systemctl start docker
+
+if [ -d "addressbook" ]
+then 
+  echo "repo is already cloned"
+  cd addressbook/
+  git pull origin master 
+else
+    git clone https://github.com/nikhilk1234/demo.git
+    cd addressbook/
+fi
+sudo docker build -t image /home/ec2-user/addressbook
+
+
+
+
